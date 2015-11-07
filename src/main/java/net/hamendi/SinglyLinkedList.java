dbe794df4618ce2295be7ea6c8864d06b82fc635
hamendi <hamendi@gmail.com>
2015-11-07 11:02:54 +0100

    Update SinglyLinkedList.java

--- src/main/java/net/hamendi/SinglyLinkedList.java
+++ src/main/java/net/hamendi/SinglyLinkedList.java
@@ -2,27 +2,15 @@ package net.hamendi;
 
 /**
  *
- * Interface that defines the requirements for
- * the Big Data Cloud Service Technical Exercise task:
- * 
- * Implement multi-thread singly linked list in Java with following methods:
+ * A multi-threaded singly linked list in Java with following methods:
  * . push(Object o) add node to the end of list
  * . pop() remove last node
  * . insertAfter(Object o, Object after) insert node next to after node
  *
- * These methods are usually associated with stacks or LIFO queues, but most
- * existing implementations are either doubly linked lists or perform push and
- * pop on the head for performance reasons. The requirements do specify that the
- * implementation perform push and pop at the tail end and also do not specify a
- * return type for any of the methods, allowing me the freedom to decide. I have
- * therefore provided conventional return types for similar methods often found
- * in similar collection implementations.
- *
- * Although the requirements do not limit the list to a homogenous structure, the
- * interface will allow usage that restricts the list to a specific type through
- * generics or, as the requirements above, the generic type Object can be used to
- * instantiate a heterogenous list.
- *
+ * Performing push and pop at the tail end is usually associated with stacks or 
+ * LIFO queues, but most existing implementations are either doubly linked lists 
+ * or perform push and pop on the head for performance reasons.
+ * 
  * @author hamendi
  *
  */
